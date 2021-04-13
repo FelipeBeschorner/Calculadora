20a2ba4a60987602ec9883454132a5774ed69884
Felipe Beschorner <felipebeschorner@hotmail.com>
2021-04-13 17:13:17 -0300

    primeira versão do projeto

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Calculadora {
+    public partial class Calculadora : Form {
+        public Calculadora() {
+            InitializeComponent();
+        }
+
+        List<int> calculo = new List<int>();
+        int numeroSalvo = 0;
+        string operadorSalvo;
+        private void salvaValorBotao( object sender , EventArgs e ) {
+            Button valorBotao = (Button)sender;
+            int numero;
+            bool result = Int32.TryParse( valorBotao.Text , out numero );
+            if( result ) {
+                numeroSalvo = numeroSalvo * 10 + numero;
+            } else {
+                calcMethod( valorBotao.Text );
+            }
+        }
+        private void calcMethod(string operador) {
+            calculo.Add( numeroSalvo );
+            numeroSalvo = 0;
+            switch( operadorSalvo ) {
+                case "/":
+                    break;
+                case "*":
+                    break;
+                case "+":
+                    break;
+                case "-":
+                    break;
+                default:
+                    operadorSalvo = operador;
+                    break;
+            }
+        }
+            
+    }
+}
